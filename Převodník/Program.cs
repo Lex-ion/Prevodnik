d907fa61fa9877483c6484732256756d5bc6fe9c
urbantomas <urbantomas@L208PC11>
2022-11-29 09:31:32 +0100

    Přidejte soubory projektu.

---
+++ Převodník/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Převodník
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("zadej č. v des. soustavě");
+            int vstup = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("zadej cílovou soustavu");
+            int soustava = Convert.ToInt32(Console.ReadLine());
+            int vystup = vstup;
+            
+            do
+            {
+                vystup=vstup%soustava;
+                vstup=vstup/soustava;
+                Console.WriteLine(vystup);
+            } while (vstup!=0);
+
+            Console.ReadLine();
+
+        }
+    }
+}
